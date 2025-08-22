cf5e5d6c86c8302e5a22cf5584ecdb4b9e34953a
franců <francupatrik@seznam.cz>
2025-08-22 22:06:06 +0200

    Fixes

--- viewer/views.py
+++ viewer/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from viewer.models import Ebook
+
 
 # Create your views here.
+''' ----- EBOOK SECTION -----'''
 
-def landing_page(request):
-    return render(request, 'landing_page.html')
+# Class used for adding new ebooks to the website
+class EbookCreateView(CreateView):
+    model = Ebook
+    fields = '__all__'
+    template_name = 'EbookCreateView.html'
+    success_url = '/'
+
