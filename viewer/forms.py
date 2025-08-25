2bf0ff5e5ef0f94f7c6aa85cac9f503bc5572730
franců <francupatrik@seznam.cz>
2025-08-25 21:36:16 +0200

    FIX EbookCreateForm in forms.py FIX EbookCreateView in views.py

--- viewer/forms.py
+++ viewer/forms.py
@@ -1,33 +1,40 @@
 from django import forms
-
+from .models import Ebook
 
 
 class EbookCreateForm(forms.ModelForm):
     class Meta:
+        model = Ebook
         fields = ["name",
                   "cover_image",
-                  "created_at"
+                  "created_at",
                   "updated_at",
                   "description",
                   "price_amount",
                   "price_currency",
-                  "file"
+                  "file",
                   ]
 
         widgets = {
             "created_at": forms.DateTimeInput(
-                attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"
+                attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M",
             ),
             "updated_at": forms.DateTimeInput(
-                attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M"
+                attrs={"type": "datetime-local"}, format="%Y-%m-%dT%H:%M",
             )
         }
 
         labels = {"name": "Name",
                   "description": "Description",
                   "price_amount": "Price",
-        }
+                  "price_currency": "Currency",
+                  "cover_image": "Cover Image",
+                  "file": "E-book File"
+                  }
         help_texts = {"name": "Enter name of your E-book",
                       "description": "Enter description of your E-book",
                       "price_amount": "Enter price of your E-book",
+                      "price_currency": "Select currency for the price",
+                      "cover_image": "Upload a cover image for your E-book",
+                      "file": "Upload your E-book file (PDF recommended)"
                       }
--- viewer/views.py
+++ viewer/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import CreateView
 from viewer.models import Ebook
+from .forms import EbookCreateForm
 
 
 # Create your views here.
@@ -8,7 +9,7 @@ from viewer.models import Ebook
 # Class used for adding new ebooks to the website
 class EbookCreateView(CreateView):
     model = Ebook
-    fields = '__all__'
+    form_class = EbookCreateForm
     template_name = 'ebook_create.html'
     success_url = '/'
 
