e4f96003ecf9c8ad09912012876f2f818fcac0c6
franců <francupatrik@seznam.cz>
2025-08-29 23:25:07 +0200

    ADD about_me.html + URL ADD contact.html + URL

--- TrainBase/urls.py
+++ TrainBase/urls.py
@@ -25,8 +25,10 @@ from viewer.views import EbookCreateView, AuthorCreateView, EbookListView, Ebook
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-
+    path('aboutme/', TemplateView.as_view(template_name='about_me.html'), name='about_me'),
     path('author/create/', AuthorCreateView.as_view(), name='author_create'),
+
+
 
     path('ebook/create/', EbookCreateView.as_view(), name='ebook_create'),
     path('ebook/list/', EbookListView.as_view(), name='ebook_list'),
