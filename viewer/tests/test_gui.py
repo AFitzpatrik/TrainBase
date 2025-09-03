615178378447652ce95a21f7f6e833adfd4da253
franců <francupatrik@seznam.cz>
2025-09-03 23:20:43 +0200

    EDIT test_add_book in test_gui.py

--- viewer/tests/test_gui.py
+++ viewer/tests/test_gui.py
@@ -1,7 +1,15 @@
 import re
-from playwright.sync_api import Page, expect
 
-def test_add_book(page: Page):
-    page.goto("http://127.0.0.1:8000/ebook/create/")
-    expect(page).to_have_title(re.compile("Add e-book"))
-    page.get_by_label("name").fill("name")
+from playwright.sync_api import sync_playwright, expect
+
+def test_add_book():
+    with sync_playwright() as p:
+        #This so we can see browser in action during the test
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto("http://127.0.0.1:8000/ebook/create/")
+        expect(page).to_have_title(re.compile("Add e-book"))
+        page.fill('input[name="name"]', "name")
+        page.select_option("#id_ebook_author", label="franta pepa jednička")
+        page.wait_for_timeout(4000)
+        browser.close()
