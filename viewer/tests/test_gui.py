8543f9058eec7bdbffa4fdcd4ec03fcf5cea6f60
franců <francupatrik@seznam.cz>
2025-09-04 12:33:44 +0200

    test_gui.py ADD description input

--- viewer/tests/test_gui.py
+++ viewer/tests/test_gui.py
@@ -9,9 +9,18 @@ def test_add_book():
         page = browser.new_page()
         page.goto("http://127.0.0.1:8000/ebook/create/")
         expect(page).to_have_title(re.compile("Add e-book"))
-        page.fill('input[name="name"]', "name")
+
+        page.fill('#id_name', "name")
         page.select_option("#id_ebook_author", label="franta pepa jednička")
-        page.wait_for_selector('textarea[name="description"]')
-        page.fill('textarea[name="description"]', "Description")
+
+        page.wait_for_selector('#id_description')
+        page.fill('#id_description', "Description")
+
+        page.fill('#id_price_amount', '199')
+
+
+
+
+
         page.wait_for_timeout(4000)
         browser.close()
