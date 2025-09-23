c944f013a781eeaead90372df65d429d44bbde01
franců <francupatrik@seznam.cz>
2025-09-23 21:26:23 +0200

    ADD accounts app.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField()
+    avatar= models.ImageField(upload_to='images/avatars', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
